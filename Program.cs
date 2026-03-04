9a4291593a6f124723d238b5aafb9ad21a7a9799
Zejd Avdic <zejd05@gmail.com>
2026-03-04 10:53:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Grupp_14_Lagerhantering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
